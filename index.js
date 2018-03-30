b38c7e9dab05bfb6d370957d3fd451c8a77d0b8f
Lydia Batchelor <lydia.l.batchelor@gmail.com>
2018-03-30 14:20:57 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/api/main', (req, res) => {
+    console.log("Success");
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`Listening on ${port}`);
