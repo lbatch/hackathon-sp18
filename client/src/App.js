540234e91e064e0365cf963c9a16c80fb7021863
Zachary Anderson <zanderson3j@gmail.com>
2018-03-30 21:25:36 -0400

    deleted unneeded files from react and started organizing

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react';
-import './App.css';
+import Banner from './Banner';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Banner />
+
       </div>
     );
   }
---
+++ client/src/Banner.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Banner.css';
+
+const banner = (props) => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Optimus Time</h1>
+        <p className="App-intro">
+          ...Instructions Here...
+        </p>
+      </header>
+    </div>
+  )
+}
+
+export default banner;
