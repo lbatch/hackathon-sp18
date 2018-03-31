281b6d4f46b5a54ee623c884bc59a6cdc4b93b11
Zachary Anderson <zanderson3j@gmail.com>
2018-03-30 23:38:28 -0400

    beginings of a calendar

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,66 @@
 import React, { Component } from 'react';
 import Banner from './Banner';
+import Calendar from './Calendar';
+import './App.css'
 
 class App extends Component {
   constructor(props) {
+    let currentMonth = new Date().getMonth();
+    let currentYear = new Date().getFullYear();
     super(props);
-    this.state = {};
+    this.state = {
+      displayMonth: {
+        month: currentMonth,
+        year: currentYear
+      }
+    };
   }
 
+  lastMonthHandler = () => {
+    let newMonth;
+    let newYear = this.state.displayMonth.year;
+    if(this.state.displayMonth.month === 0) {
+      newMonth = 11;
+      newYear = this.state.displayMonth.year - 1;
+    } else {
+      newMonth = this.state.displayMonth.month - 1;
+    }
+    this.setState({
+      ...this.state,
+      displayMonth: {
+        month: newMonth,
+        year: newYear
+      }
+    });
+  }
+
+  nextMonthHandler = () => {
+    let newMonth;
+    let newYear = this.state.displayMonth.year;
+    if(this.state.displayMonth.month === 11) {
+      newMonth = 0;
+      newYear = this.state.displayMonth.year + 1;
+    } else {
+      newMonth = this.state.displayMonth.month + 1;
+    }
+    this.setState({
+      ...this.state,
+      displayMonth: {
+        month: newMonth,
+        year: newYear
+      }
+    });
+  }
 
   render() {
     return (
       <div>
         <Banner />
-
+        <Calendar display={this.state.displayMonth}/>
+        <div>
+          <button className="changeDisplay" onClick={this.lastMonthHandler}>Prev</button>
+          <button className="changeDisplay" onClick={this.nextMonthHandler}>Next</button>
+        </div>
       </div>
     );
   }
--- client/src/Calendar.js
+++ client/src/Calendar.js
@@ -1,6 +1,92 @@
 import React from 'react';
+import './Calendar.css'
 
 
 const calendar = (props) => {
-  
+  let dates = [];
+  let datesSize = 41;
+  let days = daysInAMonth(props.display.month, props.display.year);
+  let firstDay = firstDayOfMonth(props.display.month, props.display.year);
+  const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+  let dayCount = 0;
+  for(let i = 0; i < datesSize; ++i) {
+    if(i < firstDay || dayCount >= days) {
+      dates[i] = '';
+    }
+    else {
+      dates[i] = ++dayCount;
+    }
+  }
+
+  return (
+    <div className="calendar">
+      <h1>{monthNames[props.display.month]} {props.display.year}</h1>
+      <div className="boardRow">
+        <button className="day">{dates[0]}</button>
+        <button className="day">{dates[1]}</button>
+        <button className="day">{dates[2]}</button>
+        <button className="day">{dates[3]}</button>
+        <button className="day">{dates[4]}</button>
+        <button className="day">{dates[5]}</button>
+        <button className="day">{dates[6]}</button>
+      </div>
+      <div className="boardRow">
+        <button className="day">{dates[7]}</button>
+        <button className="day">{dates[8]}</button>
+        <button className="day">{dates[9]}</button>
+        <button className="day">{dates[10]}</button>
+        <button className="day">{dates[11]}</button>
+        <button className="day">{dates[12]}</button>
+        <button className="day">{dates[13]}</button>
+      </div>
+    <div className="boardRow">
+        <button className="day">{dates[14]}</button>
+        <button className="day">{dates[15]}</button>
+        <button className="day">{dates[16]}</button>
+        <button className="day">{dates[17]}</button>
+        <button className="day">{dates[18]}</button>
+        <button className="day">{dates[19]}</button>
+        <button className="day">{dates[20]}</button>
+      </div>
+      <div className="boardRow">
+        <button className="day">{dates[21]}</button>
+        <button className="day">{dates[22]}</button>
+        <button className="day">{dates[23]}</button>
+        <button className="day">{dates[24]}</button>
+        <button className="day">{dates[25]}</button>
+        <button className="day">{dates[26]}</button>
+        <button className="day">{dates[27]}</button>
+      </div>
+      <div className="boardRow">
+        <button className="day">{dates[28]}</button>
+        <button className="day">{dates[29]}</button>
+        <button className="day">{dates[30]}</button>
+        <button className="day">{dates[31]}</button>
+        <button className="day">{dates[32]}</button>
+        <button className="day">{dates[33]}</button>
+        <button className="day">{dates[34]}</button>
+      </div>
+      <div className="boardRow">
+        <button className="day">{dates[35]}</button>
+        <button className="day">{dates[36]}</button>
+        <button className="day">{dates[37]}</button>
+        <button className="day">{dates[38]}</button>
+        <button className="day">{dates[39]}</button>
+        <button className="day">{dates[40]}</button>
+        <button className="day">{dates[41]}</button>
+      </div>
+    </div>
+  )
+
 }
+
+const daysInAMonth = (month, year) => {
+  return new Date(year, month + 1, 0).getDate();
+}
+
+const firstDayOfMonth = (month, year) => {
+  return new Date(year, month, 1).getDay();
+}
+
+export default calendar;
